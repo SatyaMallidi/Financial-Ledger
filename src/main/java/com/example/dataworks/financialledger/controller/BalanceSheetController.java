918efffc61593a8e19cec7bbb753f74fcad06875
satya <satyanarayanareddy31245@gmail.com>
2024-05-18 16:34:23 -0500

    S#Testing commit

--- src/main/java/com/example/dataworks/financialledger/controller/BalanceSheetController.java
+++ src/main/java/com/example/dataworks/financialledger/controller/BalanceSheetController.java
@@ -1,13 +1,5 @@
 package com.example.dataworks.financialledger.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-
-import com.example.dataworks.financialledger.service.BalanceSheetService;
-
 public class BalanceSheetController {
-
-  @Autowired
-  private BalanceSheetService balanceSheetService;
   
-    
 }
